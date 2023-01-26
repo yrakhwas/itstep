1c6a77c756926acc04fdaff350a9c2ca8505dd37
yrakhwas <yrakhwas@gmail.com>
2023-01-26 23:07:12 +0200

    New class

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+class Student:
+    def __init__(self):
+        print("Hello i`m a new student")
+    def InfoPrint(self):
+        print("My name is ...")
+
+nick = Student()
+nick.InfoPrint()
+
 print("Hello world")
